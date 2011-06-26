f5442ecc39578e32fc747cb510f6ff7fd2b7645a
Randall Degges <rdegges@gmail.com>
2011-06-26 02:25:49 -0700

    Adding in django loader hooks.
    This way, we can properly use autodoc / viewcode in our docs.

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,12 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../django_twilio'))
+sys.path.insert(1, os.path.abspath('../test_project'))
+
+import settings
+from django.core.management import setup_environ
+setup_environ(settings)
 
 # -- General configuration -----------------------------------------------------
 
