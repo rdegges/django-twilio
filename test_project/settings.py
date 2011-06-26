fd54d6b0e1f7259c589c954c050001d23f722fb9
Randall Degges <rdegges@gmail.com>
2011-06-26 14:13:43 -0700

    Enabling admin in test project.

--- test_project/settings.py
+++ test_project/settings.py
@@ -116,9 +116,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 	'django_twilio',
 )
 
