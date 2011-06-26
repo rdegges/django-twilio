a03aeaa3f105e12ec117862e502ab1b20bd6283a
Randall Degges <rdegges@gmail.com>
2011-06-26 14:19:16 -0700

    Adding Caller.__unicode__ method.

--- django_twilio/models.py
+++ django_twilio/models.py
@@ -5,3 +5,6 @@ class Caller(models.Model):
 	"""A caller is defined uniquely by their phone number."""
 	blacklisted = models.BooleanField()
 	phone_number = models.CharField(max_length=20, unique=True)
+
+	def __unicode__(self):
+		return self.phone_number + (' (blacklisted) ' if self.blacklisted else '')
