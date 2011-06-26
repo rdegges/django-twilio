186393f67b2ac13f8b98b50193c414107311eb1b
Randall Degges <rdegges@gmail.com>
2011-06-26 14:35:06 -0700

    Addinng Sphinx markup for Caller model.
    This documents the params.

--- django_twilio/models.py
+++ django_twilio/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 
 class Caller(models.Model):
-	"""A caller is defined uniquely by their phone number."""
+	"""A caller is defined uniquely by their phone number. Each caller has the
+	following fields:
+
+	:param bool blacklisted: Desginates whether the caller can use our
+		services.
+	:param char phone_number: Unique phone number in `E.164
+		<http://en.wikipedia.org/wiki/E.164>`_ format.
+	"""
 	blacklisted = models.BooleanField()
 	phone_number = models.CharField(max_length=20, unique=True)
 
