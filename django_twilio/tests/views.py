6e053f20933250e3dc8c1fdd99974d5c4b3b0b43
Randall Degges <rdegges@gmail.com>
2011-07-02 23:28:39 -0700

    Adding in all test views.

--- django_twilio/tests/views.py
+++ django_twilio/tests/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
 
+from twilio import Response
+
 from django_twilio.decorators import twilio_view
+
+
+
+@twilio_view
+def response_view(request):
+	"""A simple test view that returns a HttpResponse object."""
+	return HttpResponse('<Response><Sms>Hi!</Sms></Response>',
+			mimetype='text/xml')
 
 
 @twilio_view
@@ -10,7 +20,8 @@ def str_view(request):
 
 
 @twilio_view
-def response_view(request):
-	"""A simple test view that returns a HttpResponse object."""
-	return HttpResponse('<Response><Sms>Hi!</Sms></Response>',
-			mimetype='text/xml')
+def verb_view(request):
+	"""A simple test view that returns a ``twilio.Verb`` object."""
+	r = Response()
+	r.addReject()
+	return r
