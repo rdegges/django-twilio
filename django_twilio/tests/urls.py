12b40184c02376ff4d84c4a3263c107603ea0ddf
Randall Degges <rdegges@gmail.com>
2011-07-04 16:09:40 -0700

    Adding comments to our ``django_twilio.tests.urls`` module.

--- django_twilio/tests/urls.py
+++ django_twilio/tests/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns, url
 
 
 urlpatterns = patterns('django_twilio.tests.views',
+	# Test URLs for our ``django_twilio.decorators`` module.
 	url(r'^tests/response_view/$', 'response_view'),
 	url(r'^tests/str_view/$', 'str_view'),
 	url(r'^tests/verb_view/$', 'verb_view'),
