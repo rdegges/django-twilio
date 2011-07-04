cad7b9ba546bbd8686030971bd7c16a2a5324661
Randall Degges <rdegges@gmail.com>
2011-07-04 15:51:56 -0700

    Updating ``django_twilio.tests.urls``.
    Adding a more reasonable test URL prefix. Doesn't really effect anything, but
    looks much nicer..

--- django_twilio/tests/urls.py
+++ django_twilio/tests/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, url
 
 
 urlpatterns = patterns('django_twilio.tests.views',
-	url(r'^test/response_view/$', 'response_view'),
-	url(r'^test/str_view/$', 'str_view'),
-	url(r'^test/verb_view/$', 'verb_view'),
+	url(r'^tests/response_view/$', 'response_view'),
+	url(r'^tests/str_view/$', 'str_view'),
+	url(r'^tests/verb_view/$', 'verb_view'),
 )
