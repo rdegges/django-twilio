38f9c08b02352cf0b1a4aa11a06977fe033c2438
Randall Degges <rdegges@gmail.com>
2011-07-04 16:11:59 -0700

    Updating test urls for clarity.

--- django_twilio/tests/urls.py
+++ django_twilio/tests/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('django_twilio.tests.views',
 	# Test URLs for our ``django_twilio.decorators`` module.
-	url(r'^tests/response_view/$', 'response_view'),
-	url(r'^tests/str_view/$', 'str_view'),
-	url(r'^tests/verb_view/$', 'verb_view'),
+	url(r'^tests/decorators/response_view/$', 'response_view'),
+	url(r'^tests/decorators/str_view/$', 'str_view'),
+	url(r'^tests/decorators/verb_view/$', 'verb_view'),
 )
