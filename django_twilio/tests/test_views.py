c78388b838f7ed5c8523f2b5414c2650e20d71e1
Randall Degges <rdegges@gmail.com>
2011-07-11 00:01:06 -0700

    Adding unit tests for ``django_twilio.views.gather``.

--- django_twilio/tests/test_views.py
+++ django_twilio/tests/test_views.py
@@ -5,7 +5,7 @@ from base64 import encodestring
 from django.conf import settings
 from django.test import Client, RequestFactory, TestCase
 
-from django_twilio.views import conference, play, say
+from django_twilio.views import conference, gather, play, say
 
 
 class ConferenceTestCase(TestCase):
@@ -35,6 +35,30 @@ class ConferenceTestCase(TestCase):
     def test_conference_with_name(self):
         request = self.factory.post(self.conf_uri, HTTP_X_TWILIO_SIGNATURE=self.signature)
         self.assertEquals(conference(request, name='a').status_code, 200)
+
+
+class GatherTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.factory = RequestFactory()
+
+        # Test URIs.
+        self.uri = 'http://testserver/tests/views'
+        self.gather_uri = '/tests/views/gather/'
+
+        # Guarantee a value for the required configuration settings after each
+        # test case.
+        settings.TWILIO_ACCOUNT_SID = 'xxx'
+        settings.TWILIO_AUTH_TOKEN = 'xxx'
+
+        # Pre-calculate twilio signatures for our test views.
+        self.signature = encodestring(new(settings.TWILIO_AUTH_TOKEN,
+                '%s/gather/' % self.uri, sha1).digest()).strip()
+
+    def test_gather(self):
+        request = self.factory.post(self.gather_uri, HTTP_X_TWILIO_SIGNATURE=self.signature)
+	self.assertEquals(gather(request).status_code, 200)
 
 
 class SayTestCase(TestCase):
