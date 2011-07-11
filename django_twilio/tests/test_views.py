9fdc67a2cab59338739acc5dd47df3ac4256eb5c
Randall Degges <rdegges@gmail.com>
2011-07-10 23:10:46 -0700

    Refactoring ``test_views`` module.
    Now we separate our view test cases out.

--- django_twilio/tests/test_views.py
+++ django_twilio/tests/test_views.py
@@ -18,6 +18,33 @@ class ConferenceTestCase(TestCase):
         # Test URIs.
         self.uri = 'http://testserver/tests/views'
         self.conf_uri = '/tests/views/conference/'
+
+        # Guarantee a value for the required configuration settings after each
+        # test case.
+        settings.TWILIO_ACCOUNT_SID = 'xxx'
+        settings.TWILIO_AUTH_TOKEN = 'xxx'
+
+        # Pre-calculate twilio signatures for our test views.
+        self.signature = encodestring(new(settings.TWILIO_AUTH_TOKEN,
+                '%s/conference/' % self.uri, sha1).digest()).strip()
+
+    def test_conference_no_name(self):
+        request = self.factory.post(self.conf_uri, HTTP_X_TWILIO_SIGNATURE=self.signature)
+        self.assertRaises(TypeError, conference, request)
+
+    def test_conference_with_name(self):
+        request = self.factory.post(self.conf_uri, HTTP_X_TWILIO_SIGNATURE=self.signature)
+        self.assertEquals(conference(request, name='a').status_code, 200)
+
+
+class SayTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.factory = RequestFactory()
+
+        # Test URIs.
+        self.uri = 'http://testserver/tests/views'
         self.say_uri = '/tests/views/say/'
 
         # Guarantee a value for the required configuration settings after each
@@ -26,23 +53,13 @@ class ConferenceTestCase(TestCase):
         settings.TWILIO_AUTH_TOKEN = 'xxx'
 
         # Pre-calculate twilio signatures for our test views.
-        self.conf_signature = encodestring(new(settings.TWILIO_AUTH_TOKEN,
-                '%s/conference/' % self.uri, sha1).digest()).strip()
-        self.say_signature = encodestring(new(settings.TWILIO_AUTH_TOKEN,
+        self.signature = encodestring(new(settings.TWILIO_AUTH_TOKEN,
                 '%s/say/' % self.uri, sha1).digest()).strip()
 
-    def test_conference_no_name(self):
-        request = self.factory.post(self.conf_uri, HTTP_X_TWILIO_SIGNATURE=self.conf_signature)
-        self.assertRaises(TypeError, conference, request)
-
-    def test_conference_with_name(self):
-        request = self.factory.post(self.conf_uri, HTTP_X_TWILIO_SIGNATURE=self.conf_signature)
-        self.assertEquals(conference(request, name='a').status_code, 200)
-
     def test_say_no_text(self):
-        request = self.factory.post(self.say_uri, HTTP_X_TWILIO_SIGNATURE=self.say_signature)
+        request = self.factory.post(self.say_uri, HTTP_X_TWILIO_SIGNATURE=self.signature)
 	self.assertRaises(TypeError, say, request)
 
     def test_say_with_text(self):
-        request = self.factory.post(self.say_uri, HTTP_X_TWILIO_SIGNATURE=self.say_signature)
+        request = self.factory.post(self.say_uri, HTTP_X_TWILIO_SIGNATURE=self.signature)
 	self.assertEquals(say(request, text='hi').status_code, 200)
