365013bb052be0e1fca9a7b7d9cba01518a3144b
Randall Degges <rdegges@gmail.com>
2011-07-20 19:41:29 -0700

    Re-ordering test views by order.

--- django_twilio/tests/test_views.py
+++ django_twilio/tests/test_views.py
@@ -8,83 +8,6 @@ from django.test import Client, RequestFactory, TestCase
 from django_twilio.views import conference, gather, play, record, say
 
 
-class ConferenceTestCase(TestCase):
-    """Run tests against the ``conference`` view."""
-
-    def setUp(self):
-        self.client = Client()
-        self.factory = RequestFactory()
-
-        # Test URIs.
-        self.uri = 'http://testserver/tests/views'
-        self.conf_uri = '/tests/views/conference/'
-
-        # Guarantee a value for the required configuration settings after each
-        # test case.
-        settings.TWILIO_ACCOUNT_SID = 'xxx'
-        settings.TWILIO_AUTH_TOKEN = 'xxx'
-
-        # Pre-calculate twilio signatures for our test views.
-        self.signature = encodestring(new(settings.TWILIO_AUTH_TOKEN,
-                '%s/conference/' % self.uri, sha1).digest()).strip()
-
-    def test_conference_no_name(self):
-        request = self.factory.post(self.conf_uri, HTTP_X_TWILIO_SIGNATURE=self.signature)
-        self.assertRaises(TypeError, conference, request)
-
-    def test_conference_with_name(self):
-        request = self.factory.post(self.conf_uri, HTTP_X_TWILIO_SIGNATURE=self.signature)
-        self.assertEquals(conference(request, name='a').status_code, 200)
-
-
-class GatherTestCase(TestCase):
-
-    def setUp(self):
-        self.client = Client()
-        self.factory = RequestFactory()
-
-        # Test URIs.
-        self.uri = 'http://testserver/tests/views'
-        self.gather_uri = '/tests/views/gather/'
-
-        # Guarantee a value for the required configuration settings after each
-        # test case.
-        settings.TWILIO_ACCOUNT_SID = 'xxx'
-        settings.TWILIO_AUTH_TOKEN = 'xxx'
-
-        # Pre-calculate twilio signatures for our test views.
-        self.signature = encodestring(new(settings.TWILIO_AUTH_TOKEN,
-                '%s/gather/' % self.uri, sha1).digest()).strip()
-
-    def test_gather(self):
-        request = self.factory.post(self.gather_uri, HTTP_X_TWILIO_SIGNATURE=self.signature)
-	self.assertEquals(gather(request).status_code, 200)
-
-
-class RecordTestCase(TestCase):
-
-    def setUp(self):
-        self.client = Client()
-        self.factory = RequestFactory()
-
-        # Test URIs.
-        self.uri = 'http://testserver/tests/views'
-        self.record_uri = '/tests/views/record/'
-
-        # Guarantee a value for the required configuration settings after each
-        # test case.
-        settings.TWILIO_ACCOUNT_SID = 'xxx'
-        settings.TWILIO_AUTH_TOKEN = 'xxx'
-
-        # Pre-calculate twilio signatures for our test views.
-        self.signature = encodestring(new(settings.TWILIO_AUTH_TOKEN,
-                '%s/record/' % self.uri, sha1).digest()).strip()
-
-    def test_record(self):
-        request = self.factory.post(self.record_uri, HTTP_X_TWILIO_SIGNATURE=self.signature)
-	self.assertEquals(record(request).status_code, 200)
-
-
 class SayTestCase(TestCase):
 
     def setUp(self):
@@ -139,3 +62,79 @@ class PlayTestCase(TestCase):
     def test_play_with_url(self):
         request = self.factory.post(self.play_uri, HTTP_X_TWILIO_SIGNATURE=self.signature)
 	self.assertEquals(play(request, url='http://b.com/b.wav').status_code, 200)
+
+
+class GatherTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.factory = RequestFactory()
+
+        # Test URIs.
+        self.uri = 'http://testserver/tests/views'
+        self.gather_uri = '/tests/views/gather/'
+
+        # Guarantee a value for the required configuration settings after each
+        # test case.
+        settings.TWILIO_ACCOUNT_SID = 'xxx'
+        settings.TWILIO_AUTH_TOKEN = 'xxx'
+
+        # Pre-calculate twilio signatures for our test views.
+        self.signature = encodestring(new(settings.TWILIO_AUTH_TOKEN,
+                '%s/gather/' % self.uri, sha1).digest()).strip()
+
+    def test_gather(self):
+        request = self.factory.post(self.gather_uri, HTTP_X_TWILIO_SIGNATURE=self.signature)
+	self.assertEquals(gather(request).status_code, 200)
+
+
+class RecordTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.factory = RequestFactory()
+
+        # Test URIs.
+        self.uri = 'http://testserver/tests/views'
+        self.record_uri = '/tests/views/record/'
+
+        # Guarantee a value for the required configuration settings after each
+        # test case.
+        settings.TWILIO_ACCOUNT_SID = 'xxx'
+        settings.TWILIO_AUTH_TOKEN = 'xxx'
+
+        # Pre-calculate twilio signatures for our test views.
+        self.signature = encodestring(new(settings.TWILIO_AUTH_TOKEN,
+                '%s/record/' % self.uri, sha1).digest()).strip()
+
+    def test_record(self):
+        request = self.factory.post(self.record_uri, HTTP_X_TWILIO_SIGNATURE=self.signature)
+	self.assertEquals(record(request).status_code, 200)
+
+
+class ConferenceTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.factory = RequestFactory()
+
+        # Test URIs.
+        self.uri = 'http://testserver/tests/views'
+        self.conf_uri = '/tests/views/conference/'
+
+        # Guarantee a value for the required configuration settings after each
+        # test case.
+        settings.TWILIO_ACCOUNT_SID = 'xxx'
+        settings.TWILIO_AUTH_TOKEN = 'xxx'
+
+        # Pre-calculate twilio signatures for our test views.
+        self.signature = encodestring(new(settings.TWILIO_AUTH_TOKEN,
+                '%s/conference/' % self.uri, sha1).digest()).strip()
+
+    def test_conference_no_name(self):
+        request = self.factory.post(self.conf_uri, HTTP_X_TWILIO_SIGNATURE=self.signature)
+        self.assertRaises(TypeError, conference, request)
+
+    def test_conference_with_name(self):
+        request = self.factory.post(self.conf_uri, HTTP_X_TWILIO_SIGNATURE=self.signature)
+        self.assertEquals(conference(request, name='a').status_code, 200)
