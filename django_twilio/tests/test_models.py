0ab57ebe0347a41bd9fac7ba81e51e6994821aec
Randall Degges <rdegges@gmail.com>
2011-09-05 23:20:12 -0700

    Modularizing test cases for Caller.__unicode__.

--- django_twilio/tests/test_models.py
+++ django_twilio/tests/test_models.py
@@ -13,7 +13,17 @@ class CallerTestCase(TestCase):
 
     def test_has_unicode(self):
         self.assertTrue(isinstance(self.caller.__unicode__, MethodType))
+
+    def test_unicode_returns_str(self):
         self.assertTrue(isinstance(self.caller.__unicode__(), str))
+
+    def test_unicode_doesnt_contain_blacklisted(self):
+        self.assertFalse('blacklisted' in self.caller.__unicode__())
+
+    def test_unicode_contains_blacklisted(self):
+        self.caller.blacklisted = True
+        self.caller.save()
+        self.assertTrue('blacklisted' in self.caller.__unicode__())
 
     def tearDown(self):
         self.caller.delete()
