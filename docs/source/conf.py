bf9d089c0c46969534da75bcb54ec98fd200bd11
Randall Degges <rdegges@gmail.com>
2011-09-07 18:48:01 -0700

    Re-building documentation structure.
    Using the separate build / source directories for our Sphinx docs, since as the
    documentation is growing, it's becoming uglier and uglier to have it all in one
    place.
    
    Eventually I'd like to move to a heiarchal documentation sorta scheme, so we can
    group various chapters (if you will) into their own subfolders.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # django-twilio documentation build configuration file, created by
-# sphinx-quickstart on Sun Jun 26 02:19:29 2011.
+# sphinx-quickstart on Wed Sep  7 18:43:55 2011.
 #
 # This file is execfile()d with the current directory set to its containing dir.
 #
@@ -16,8 +16,8 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../django_twilio'))
-sys.path.insert(1, os.path.abspath('../test_project'))
+sys.path.insert(0, os.path.abspath('../../django_twilio'))
+sys.path.insert(1, os.path.abspath('../../test_project'))
 
 import settings
 from django.core.management import setup_environ
@@ -30,7 +30,7 @@ setup_environ(settings)
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -69,7 +69,7 @@ release = '0.1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = []
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
