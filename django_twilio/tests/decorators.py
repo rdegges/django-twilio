eb8f52277d0b025896b293af39d72310ec563d23
Randall Degges <rdegges@gmail.com>
2011-10-09 13:02:15 -0700

    twilio -> Twilio

--- django_twilio/tests/decorators.py
+++ django_twilio/tests/decorators.py
@@ -28,7 +28,7 @@ class TwilioViewTestCase(TestCase):
         conf.TWILIO_ACCOUNT_SID = 'xxx'
         conf.TWILIO_AUTH_TOKEN = 'xxx'
 
-        # Pre-calculate twilio signatures for our test views.
+        # Pre-calculate Twilio signatures for our test views.
         self.response_signature = encodestring(new(conf.TWILIO_AUTH_TOKEN,
                 '%s/response_view/' % self.uri, sha1).digest()).strip()
         self.str_signature = encodestring(new(conf.TWILIO_AUTH_TOKEN,
