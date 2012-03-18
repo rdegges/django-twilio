0dec1795df871f94e1c44286d4e4572505c9a466
Randall Degges <rdegges@gmail.com>
2012-03-17 20:28:37 -0700

    Updating copyright year.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'django-twilio'
-copyright = u'2011, Randall Degges'
+copyright = u'2012, Randall Degges'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
