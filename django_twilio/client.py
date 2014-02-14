236ff8292543d90a2feb8178e67ef0a625f8af72
Paul Hallett <hello@phalt.co>
2014-02-14 09:08:25 -0800

    Support for environment variables

--- django_twilio/client.py
+++ django_twilio/client.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 
 """Twilio REST client helpers."""
-
 from django_twilio import settings
+import os
 
 from twilio.rest import TwilioRestClient
 
-
-twilio_client = TwilioRestClient(
-    settings.TWILIO_ACCOUNT_SID,
-    settings.TWILIO_AUTH_TOKEN, version='2010-04-01')
+if 'TWILIO_ACCOUNT_SID' and 'TWILIO_AUTH_TOKEN' in os.environ:
+    twilio_client = TwilioRestClient(
+        os.environ['TWILIO_ACCOUNT_SID'],
+        os.environ['TWILIO_AUTH_TOKEN'],
+        version='2010-04-01'
+    )
+else:
+    twilio_client = TwilioRestClient(
+        settings.TWILIO_ACCOUNT_SID,
+        settings.TWILIO_AUTH_TOKEN, version='2010-04-01')
--- django_twilio/settings.py
+++ django_twilio/settings.py
@@ -6,12 +6,11 @@
 from django.conf import settings
 import os
 
-TWILIO_ACCOUNT_SID = getattr(settings, 'TWILIO_ACCOUNT_SID', '')
-TWILIO_AUTH_TOKEN = getattr(settings, 'TWILIO_AUTH_TOKEN', '')
-
-if not TWILIO_ACCOUNT_SID:
+try:
+    TWILIO_ACCOUNT_SID = getattr(settings, 'TWILIO_ACCOUNT_SID', '')
+    TWILIO_AUTH_TOKEN = getattr(settings, 'TWILIO_AUTH_TOKEN', '')
+except:
     TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID'] \
         if 'TWILIO_ACCOUNT_SID' in os.environ else ''
-if not TWILIO_AUTH_TOKEN:
     TWILIO_AUTH_TOKEN = os.environ['TWILIO_ACCOUNT_SID'] \
         if 'TWILIO_ACCOUNT_SID' in os.environ else ''
