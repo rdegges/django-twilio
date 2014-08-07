a95f0b0540d06a56dac5afd8b7df28b8b671564b
Ustin Zarubin <ustin.zarubin@campusbellhops.com>
2014-08-06 20:47:55 -0400

    Adding spaces between imports

--- test_project/test_app/urls.py
+++ test_project/test_app/urls.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals, absolute_import
 
 from django.conf.urls import patterns, url
+
 from .views import BytesView, StrView, ResponseView, VerbView
 
 # Test URLs for our ``django_twilio.decorators`` module.
