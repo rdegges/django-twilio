7472c4e0686e51fbc05fedad92f9cd3fd7eb689b
Paul Hallett <hello@phalt.co>
2014-09-22 15:12:17 +0100

    Testing for MMS support

--- test_project/test_app/views.py
+++ test_project/test_app/views.py
@@ -11,7 +11,8 @@ from django.views.generic import View
 from django.utils.decorators import method_decorator
 
 from django_twilio.views import (
-    conference, dial, gather, play, record, say, sms)
+    conference, dial, gather, play, record, say, sms, message
+    )
 
 from .utils import TwilioRequestFactory
 
@@ -209,6 +210,31 @@ class SmsTestCase(TestCase):
             sms(request, message='test').status_code,
             200,
         )
+
+class MessageTestCase(TestCase):
+
+    def setUp(self):
+
+        # Test URI
+        self.message_uri = '/test_app/views/message/'
+
+        self.factory = TwilioRequestFactory(token=settings.TWILIO_AUTH_TOKEN)
+
+    def test_message_no_message(self):
+        request = self.factory.post(self.message_uri)
+        self.assertRaises(TypeError, message, request)
+
+    def test_message_with_media(self):
+        request = self.factory.post(self.message_uri)
+        self.assertEquals(
+            message(
+                request,
+                message='test',
+                media='http://i.imgur.com/Qa8GVPU.gif'
+            ).status_code,
+            200,
+        )
+
 
 
 class DialTestCase(TestCase):
