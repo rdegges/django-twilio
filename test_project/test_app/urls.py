5293ff2bd248fb5cc744752f2042a9af7f91445e
Roger Boardman <roger@bxthree.com>
2017-03-21 21:52:54 +0000

    patterns was removed in django 1.10, url patterns is also now a flat list of django.conf.urls.url() instances

--- test_project/test_app/urls.py
+++ test_project/test_app/urls.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-from .views import BytesView, StrView, ResponseView, VerbView
+from . import views
 
 # Test URLs for our ``django_twilio.decorators`` module.
-urlpatterns = patterns(
-    'test_project.test_app.views',
-    url(r'^test_app/decorators/response_view/$', 'response_view'),
-    url(r'^test_app/decorators/response_class_view/$', ResponseView.as_view),
-    url(r'^test_app/decorators/str_view/$', 'str_view'),
-    url(r'^test_app/decorators/str_class_view/$', StrView.as_view()),
-    url(r'^test_app/decorators/bytes_view/$', 'bytes_view'),
-    url(r'^test_app/decorators/bytes_class_view/$', BytesView.as_view()),
-    url(r'^test_app/decorators/verb_view/$', 'verb_view'),
-    url(r'^test_app/decorators/verb_class_view/$', VerbView.as_view())
-)
+urlpatterns = [
+    url(r'^test_app/decorators/response_view/$', views.response_view),
+    url(r'^test_app/decorators/response_class_view/$', views.ResponseView.as_view()),
+    url(r'^test_app/decorators/str_view/$', views.str_view),
+    url(r'^test_app/decorators/str_class_view/$', views.StrView.as_view()),
+    url(r'^test_app/decorators/bytes_view/$', views.bytes_view),
+    url(r'^test_app/decorators/bytes_class_view/$', views.BytesView.as_view()),
+    url(r'^test_app/decorators/verb_view/$', views.verb_view),
+    url(r'^test_app/decorators/verb_class_view/$', views.VerbView.as_view())
+]
