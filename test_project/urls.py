3edec57b43d381c3c4a0e8f3ab52448ca95c9223
Jason Held <jasonsheld@gmail.com>
2018-04-18 18:55:36 -0400

    Fix: uses urlpatterns urls in test project urls. allows admin. updates the test project settings so we can the project again locally.

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^test_app/', include('test_project.test_app.urls')),
+]
+
