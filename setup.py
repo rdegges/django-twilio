15b785429f1d06bc80633bbb6918da276f50027b
Jason Held <jasonsheld@gmail.com>
2022-03-17 23:11:28 -0400

    django 4.0 (#209)
    * django 4.0 support
    
    * moving to pytest

--- setup.py
+++ setup.py
@@ -9,25 +9,9 @@ import sys
 INSTALL_PYTHON_REQUIRES = []
 # We are intending to keep up to date with the supported Django versions.
 # For the official support, please visit:
-# https://docs.djangoproject.com/en/3.2/faq/install/#what-python-version-can-i-use-with-django and you may change the version in the URL to suit your needs, and we will try to update that here too as we upgrade with django.
-if sys.version_info[1] == 5:
-    django_python_version_install = 'Django>=2.2,<3.0',
-    INSTALL_PYTHON_REQUIRES.append(django_python_version_install)
-elif sys.version_info[1] == 6:
-    django_python_version_install = 'Django>=2.2,<3.3',
-    INSTALL_PYTHON_REQUIRES.append(django_python_version_install)
-elif sys.version_info[1] == 7:
-    django_python_version_install = 'Django>=2.2,<3.3'
-    INSTALL_PYTHON_REQUIRES.append(django_python_version_install)
-elif sys.version_info[1] == 8:
-    django_python_version_install = 'Django>=2.2.8,<3.3'
-    INSTALL_PYTHON_REQUIRES.append(django_python_version_install)
-elif sys.version_info[1] == 9:
-    # slightly too broad (3.0.11 for v3.0, and 3.1.3 for v3.1) -- may need to fix
-    django_python_version_install = 'Django>=2.2.17,<3.3'
-    INSTALL_PYTHON_REQUIRES.append(django_python_version_install)
-elif sys.version_info[1] == 10:
-    django_python_version_install = 'Django>=3.2.9,<3.3'
+# https://docs.djangoproject.com/en/4.0/faq/install/#what-python-version-can-i-use-with-django
+if sys.version_info[1] in [8, 9, 10]:
+    django_python_version_install = "Django>=4.0,<4.1"
     INSTALL_PYTHON_REQUIRES.append(django_python_version_install)
 
 setup(
@@ -68,17 +52,13 @@ setup(
     },
     classifiers=[
         'Framework :: Django',
-        'Framework :: Django :: 2.2',
-        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 4.0',
         'Intended Audience :: Developers',
         'License :: Public Domain',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
