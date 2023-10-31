cbb0047905c50a344454f8603ad88b6999179808
calebsyring <caleb@thesyrings.us>
2023-10-31 13:12:03 -0400

    Allow twilio-python 8+ in setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     # Package dependencies:
     install_requires=[
         'setuptools>=36.2',
-        'twilio>=7,<8',
+        'twilio>=7',
         'django-phonenumber-field>=0.6',
         'phonenumbers>=8.10.22',
     ] + INSTALL_PYTHON_REQUIRES,
